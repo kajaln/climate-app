98f77a6311fe1d2684b3cfcc73b0b3e66558483e
kajaln <kajal.narnaware@maximess.com>
2022-11-08 15:12:27 +0530

    Updates

---
+++ images.js
@@ -0,0 +1,15 @@
+const IMAGES = {
+    clear: require('./clear.jpg').default,
+    cloudDay: require('./cloud-day.jpg').default,
+    cloudNight: require('./cloud-night.jpg').default,
+    hazeDay: require('./haze-day.jpg').default,
+    hazeNight: require('./haze-night.jpg').default,
+    rainDay: require('./rain-day.jpg').default,
+    rainNight: require('./rain-night.jpg').default,
+    snowDay: require('./snow-night.jpg').default,
+    snowNight:require('./snow-night.jpg').default,
+    thuderstormDay: require('./thunderstorm-day.jpg').default,
+    thuderstormNight: require('./thunderstorm-night.jpg').default,
+}
+
+export default IMAGES;
